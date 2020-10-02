3f05e0a940f398505dece0b603b5dd0307674abf
ksjae <ksjae0705@hotmail.com>
2020-10-02 22:34:54 +0900

    Shift to TDD and rebase

---
+++ src/tests/class_test.py
@@ -0,0 +1,16 @@
+from ..customclass import *
+
+def test_anatomy_creation():
+    head = Head()
+    torso = Torso()
+    arm = Arm()
+    leg = Leg()
+    anatomy = Anatomy(head=head,
+                      torso=torso,
+                      arm=arm,
+                      leg=leg,)
+    anatomy.pose = Pose.standing
+    assert anatomy.head == head
+    assert anatomy.torso == torso
+    assert anatomy.arm == arm
+    assert anatomy.leg == leg
