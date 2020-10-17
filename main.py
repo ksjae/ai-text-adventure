e149b01b2af1b7a3562ffa0e2db117662a2bd87c
ksjae <ksjae0705@hotmail.com>
2020-10-18 07:40:59 +0900

    Add UI

---
+++ main.py
@@ -0,0 +1,2 @@
+from src.interface import main
+main()
---
+++ src/interface.py
@@ -0,0 +1,16 @@
+from .generator import *
+from .customclass import *
+
+def print_welcome():
+    print('-'*80)
+    print("AI Text Adventure PROTOTYPE A")
+
+def run_adventure():
+    # Initial config
+
+    # Loop
+    pass
+
+def main():
+    print_welcome()
+    run_adventure()
