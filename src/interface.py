4a6051b8649156d38bcde971993b03ca99eca150
ksjae <ksjae0705@hotmail.com>
2020-10-20 09:05:38 +0900

    Add randomizer content

--- src/interface.py
+++ src/interface.py
@@ -5,9 +5,12 @@ def print_welcome():
     print('-'*80)
     print("AI Text Adventure PROTOTYPE A")
 
+def get_random_initial_prompt():
+    return None
+
 def run_adventure():
     # Initial config
-
+    supported_fantasy_types = ["a heroic fantasy","a historical fantasy","a medieval fantasy","a sword and sorcery","a comic fantasy","an epic fantasy","a dark fantasy","a grimdark fantasy","a magical realism"]
     # Loop
     pass
 
--- src/tests/interface_test.py
+++ src/tests/interface_test.py
@@ -1,1 +1,4 @@
 from ..interface import *
+
+def test_initial_random_prompt():
+    assert get_random_initial_prompt() is not None
