dd511daec511292dac2144205da64d0e6900c137
ksjae <ksjae0705@hotmail.com>
2020-11-22 16:48:28 +0900

    tensorflow requirements fix

--- tests/interface_test.py
+++ tests/interface_test.py
@@ -1,5 +1,6 @@
-from aita.interface import *
 from aita.translation import KO
+from aita.interface import *
+
 
 def test_initial_random_prompt():
     assert len(get_random_initial_prompt('ko',KO)) > 20
